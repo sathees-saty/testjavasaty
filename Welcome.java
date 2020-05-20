5ce103cd7e6dc08363c3b08c644c91f9de9d3dc7
sathees-saty <satheeskumar.pmp@gmail.com>
2020-05-20 16:46:19 +0530

    second code commit

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+
+/** 
+ * The Welcome class implements an application that simply prints "Welcome" to standard output.
+ */
+public class Welcome {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Jenkins");
+  }
+}
